509b81f22d7e3b403f7009f3761f7fc46f289065
ashatkydyrov61@gmail.com <ashatkydyrov61@gmail.com>
2023-02-03 20:34:18 -0600

    first

---
+++ lessons/le5.py
@@ -0,0 +1,17 @@
+# Модули
+#1
+import time, math, random
+
+print(math.pi)
+
+from math import e as i
+
+# i = e
+# print(e)
+print(i)
+
+#2 Собственные модули
+from lesson_3 import Person
+per1 = Person("Asyl", "Turdaliev", 222, 2213)
+per1._s()
+#3 Внешние модули, взаимствованные, чужие, инородные
